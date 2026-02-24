5364952e5f4df7bad39be3c80ad1e869a4f00730
Fawz-Haaroon <fawzhaaroon216@gmail.com>
2026-02-24 23:45:42 +0530

    feat(codeforces): add solution for A - Divisibility Problem

--- CODEFORCES/PRACTICE/A_Divisibility_Problem.rs
+++ CODEFORCES/PRACTICE/A_Divisibility_Problem.rs
@@ -6,19 +6,16 @@ fn main() {
     let mut it = input.split_whitespace();
 
     let t: usize = it.next().unwrap().parse().unwrap();
-    let mut out = String::new();
 
     for _ in 0..t {
         let a: i64 = it.next().unwrap().parse().unwrap();
         let b: i64 = it.next().unwrap().parse().unwrap();
-        let r = a % b;
-        if r == 0 {
-            out.push_str("0\n");
+
+        let rem = a % b;
+        if rem == 0 {
+            println!("0");
         } else {
-            out.push_str(&(b - r).to_string());
-            out.push('\n');
+            println!("{}", b - rem);
         }
     }
-
-    print!("{}", out);
 }
