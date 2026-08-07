cfaf561d5ba23f274b79dd2e7dfbec1142834fd2
Fawz-Haaroon <fawzhaaroon216@gmail.com>
2026-08-07 18:30:00 +0530

    Solve Two Sum

--- LEETCODE/NC150/problems/Array-Hashing/src/bin/lc1-two-sum.rs
+++ LEETCODE/NC150/problems/Array-Hashing/src/bin/lc1-two-sum.rs
@@ -7,11 +7,11 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
         let mut map: HashMap<i32, usize> = HashMap::new();
-        for (i, val) in nums.iter().enumerate() {
-            let comp = target - *val;
+        for (i, &val) in nums.iter().enumerate() {
+            let comp = target - val;
 
             if let Some(&j) = map.get(&comp) { return vec![j as i32, i as i32]; }
-            else { map.insert(*val, i); }
+            else { map.insert(val, i); }
         }
 
         vec![]
@@ -27,3 +27,31 @@ fn main() {
     println!("indices of nums that add up to target: {:?}", ans);
 }
 
+
+
+
+/* first try
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // needed_number -> index
+        let mut map: HashMap<i32, usize> = HashMap::new();
+
+        for i in 0..nums.len() {
+            let current = nums[i];
+
+            // see if the current no. at index i is the one we needed (referencing the hashmap)
+            if let Some(&prev_index) = map.get(&current) {
+                return vec![prev_index as i32, i as i32];
+            }
+
+            // push to the hashmap what no. I need. and my index
+            let needed = target - current;
+            map.insert(needed, i);
+        }
+
+        unreachable!("The problem guarantees exactly one solution.");
+    }
+}
+*/
